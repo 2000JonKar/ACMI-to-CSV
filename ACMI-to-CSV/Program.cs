374bc34a5cca2c0018ab58a999091f06b157a03c
Jonatan Karlsson <2000jonkar@gmail.com>
2022-12-10 00:39:21 +0100

    Add project files.

---
+++ ACMI-to-CSV/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace ACMI_to_CSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lagra directory till Google Drive Desktop mappen där .acmi data kommer finnas
+            DirectoryInfo dir = new DirectoryInfo(@"G:\\My Drive\\DCS\\TacView\\Data");
+            FileInfo[] Files = dir.GetFiles("*.acmi"); // Lagra de filer som slutar i .acmi
+
+            // Efter långa förhandlingar, lite vilja, en aning våld, och jävligt med vaselin fick jag äntligen ordning på denna foreach
+            foreach (FileInfo file in Files)
+            {
+                string[] acmi = File.ReadAllLines(file.FullName); // Läs in och spara alla rader i respektive .acmi fil
+                File.WriteAllLines($"G:\\My Drive\\DCS\\TacView\\Data\\CSV\\CSV_{file}.csv", acmi); // Skapa .csv fil för att importera till Google Sheets
+                Console.WriteLine(file.Name); // Skriv ut .acmi-filnamn för att bekräfta
+            }
+            Console.ReadLine(); // Låt användare titta hur länge dom vill på resultaten
+        }
+    }
+}
