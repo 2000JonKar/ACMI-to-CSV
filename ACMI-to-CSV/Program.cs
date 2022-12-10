625e37abfff6d7dc7e72edf7c49c37a7f2624c0b
Jonatan Karlsson <2000jonkar@gmail.com>
2022-12-10 17:49:40 +0100

    Added HasFired filter to data

--- ACMI-to-CSV/Program.cs
+++ ACMI-to-CSV/Program.cs
@@ -20,11 +20,26 @@ namespace ACMI_to_CSV
                 ProcessStartInfo start = new ProcessStartInfo();
                 start.Arguments = $"-Open:\"G:\\My Drive\\DCS\\TacView\\Data\\{file}\" -ExportFlightLog:\"G:\\My Drive\\DCS\\TacView\\Data\\CSV\\{file}.csv\" -Quiet –Quit";
                 start.FileName = "C:\\Program Files (x86)\\Tacview\\Tacview.exe";
-                
                 using (Process proc = Process.Start(start))
                 {
                     proc.WaitForExit();
                 }
+
+                string path = "G:\\My Drive\\DCS\\TacView\\Data\\CSV\\" + file.Name + ".csv";
+                int lines = File.ReadLines(path).Count();
+                string[] data = File.ReadAllLines(path);
+
+                var query = from line in data where line.Contains("HasFired") && line.Contains("Enemies") select line;
+
+                List<string> newCSV = new List<string>();
+
+                foreach(string line in query)
+                {
+                    newCSV.Add(line);
+                }
+
+                File.WriteAllLines($"G:\\My Drive\\DCS\\TacView\\Data\\CSV\\CSV_{file}.csv", newCSV);
+                Console.WriteLine(file.FullName);
             }
             Console.WriteLine("End of process...");
             Console.ReadLine(); // Låt användare titta hur länge dom vill på resultaten
